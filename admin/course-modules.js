d1c5e0347055f7a48e5b080549f416371bbac164
Pradeep71437 <pradee.srinivas@gmail.com>
2024-01-06 16:33:40 +0530

    admin dashboard update

--- admin/course-modules.js
+++ admin/course-modules.js
@@ -23,6 +23,22 @@ function addModule() {
     </div>
     <input type="button" class="btn" onclick="addDescription(${moduleContainer.childElementCount})" value="ADD SECTION">
     </div>
+    <div class="form-group row">
+    <label class="col-sm-2 col-form-label">SECTION DURATION</label>
+    <div class="col-sm-7">
+        <!-- <input class="form-control" name="module_descriptions[0][]" type="text" > -->
+        <input class="form-control" name="section_time[${moduleContainer.childElementCount}][]" type="text" >
+
+    </div>
+    <!-- <input type="button" class="btn" onclick="addDescription(0)" value="ADD SECTION"> -->
+</div>
+<div class="form-group row">
+	<label class="col-sm-2 col-form-label">SECTION LINK</label>
+		<div class="col-sm-7">
+			<input class="form-control" name="section_link[${moduleContainer.childElementCount}][]" type="text" >
+
+		</div>
+</div>
     `;
     moduleContainer.appendChild(module);
 }
@@ -32,7 +48,28 @@ function addDescription(moduleIndex) {
     var descriptionsContainer = moduleContainer.getElementsByClassName('module')[moduleIndex];
     var description = document.createElement('div');
     description.innerHTML = `
-    <input class="form-control" name="module_descriptions[${moduleIndex}][]" type="text" ></input>
+    <div class="form-group row mt-3">
+    <label class="col-sm-2 col-form-label">SECTION</label>
+    <div class="col-sm-7 mt-2">
+        <input class="form-control" name="module_descriptions[${moduleIndex}][]" type="text" >
+    </div>
+    </div>
+    <div class="form-group row">
+    <label class="col-sm-2 col-form-label">SECTION DURATION</label>
+    <div class="col-sm-7">
+        <!-- <input class="form-control" name="module_descriptions[0][]" type="text" > -->
+        <input class="form-control" name="section_time[${moduleIndex}][]" type="text" >
+
+    </div>
+    <!-- <input type="button" class="btn" onclick="addDescription(0)" value="ADD SECTION"> -->
+</div>
+<div class="form-group row">
+	<label class="col-sm-2 col-form-label">SECTION LINK</label>
+		<div class="col-sm-7">
+			<input class="form-control" name="section_link[[${moduleIndex}][]" type="text" >
+
+		</div>
+</div>
         `;
     descriptionsContainer.appendChild(description);
 }
