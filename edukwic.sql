576c7607ea1f475e69b3e9e33992477384871ec3
santhosh <pcasd21ca076@gmail.com>
2024-01-13 11:17:29 +0530

    sql.change

---
+++ edukwic.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 13, 2024 at 06:46 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `edukwic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_details`
+--
+
+CREATE TABLE `course_details` (
+  `course_count` int(3) NOT NULL,
+  `course_title` varchar(35) NOT NULL,
+  `course_branch` varchar(15) DEFAULT NULL,
+  `anchor_tag` varchar(20) NOT NULL,
+  `image_src` varchar(35) NOT NULL,
+  `video_src` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `course_details`
+--
+
+INSERT INTO `course_details` (`course_count`, `course_title`, `course_branch`, `anchor_tag`, `image_src`, `video_src`) VALUES
+(3, 'Cybersecurity', 'Programming', 'index.html', 'assets/images/courses/pic3.jpg', 'https://youtu.be/rCPTa3lLCI0?si=WrffK4vTO9yI8X4D'),
+(2, 'Full-Stack Development', 'Programming', 'index.html', 'assets/images/courses/pic2.jpg', 'https://youtu.be/rCPTa3lLCI0?si=WrffK4vTO9yI8X4D'),
+(4, 'Game Development', 'Programming', 'index.html', 'assets/images/courses/pic4.jpg', 'https://youtu.be/rCPTa3lLCI0?si=WrffK4vTO9yI8X4D'),
+(1, 'Introduction to Programming', 'Programming', 'intprograming.hmtl', 'assets/images/courses/pic1.jpg', 'https://youtu.be/rCPTa3lLCI0?si=88mdrCaBO3OnAWlo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `user_email` varchar(50) NOT NULL,
+  `user_phone` int(11) NOT NULL,
+  `user_password` varchar(50) NOT NULL,
+  `role_as` tinyint(4) NOT NULL DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_phone`, `user_password`, `role_as`, `created_at`) VALUES
+(1, 'admin', 'edukwicorg@gmail.com', 0, '1234', 1, '2024-01-07 09:34:22'),
+(2, 'Student', 'student@gmail.com', 0, '123', 0, '2024-01-08 02:45:46');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `course_details`
+--
+ALTER TABLE `course_details`
+  ADD PRIMARY KEY (`course_title`),
+  ADD KEY `course_count` (`course_count`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
