af2a980483f6886ef7e8cbc1c056d1509bcafae9
Pradeep71437 <pradee.srinivas@gmail.com>
2024-01-19 17:13:32 +0530

    hii

--- forgot-password.php
+++ forgot-password.php
@@ -1,14 +1,6 @@
 <?php
 session_start();
-
-if(isset($_SESSION['auth']))
-{
-    // $_SESSION['message'] = "You are already logged in!";
-    header('Location: index.php');
-    exit();
-}
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -78,67 +70,28 @@ if(isset($_SESSION['auth']))
 		<div class="account-head" style="background-image:url(assets/images/background/bg2.jpg);">
 			<a href="index.html"><img src="assets/images/ek-logo-removebg.png" alt=""></a>
 		</div>
-        
 		<div class="account-form-inner">
-            <div class="container">
-                <?php
-                if(isset($_SESSION['message']))
-                {
-                    ?>
-                <div class="alert alert-warning alert-dismissable fade show " role="alert">
-                  <strong>Hey!</strong> <?= $_SESSION['message']; ?>
-                  <!-- <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> -->
-
-                </div>
-
-
-                <?php
-                unset($_SESSION['message']);
-                }
-                ?>
-            </div>
+            <?php
+              unset($_SESSION['status']);
+            ?>
 			<div class="account-container">
 				<div class="heading-bx left">
-					<h2 class="title-head">Login to your <span>Account</span></h2>
-					<p>Hey champ! New here? <a href="register.html">Get Membership</a> to get started</p>
+					<h2 class="title-head">Forget <span>Password</span></h2>
+					<p>Login Your Account <a href="login.html">Click here</a></p>
 				</div>	
-				<form class="contact-bx" method="post" action="phpfunctions/authcode.php">
+				<form class="contact-bx" action="password-change.php" method="post">
 					<div class="row placeani">
 						<div class="col-lg-12">
 							<div class="form-group">
 								<div class="input-group">
-									<label>Enter Email ID</label>
-									<input name="user_email" type="email" required="" class="form-control">
+									<label>Your Email Address</label>
+									<input name="umail" type="email" required="" class="form-control">
 								</div>
-							</div>
-						</div>
-						<div class="col-lg-12">
-							<div class="form-group">
-								<div class="input-group"> 
-									<label>Enter Password</label>
-									<input name="user_pass" type="password" class="form-control" required="">
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-12">
-							<div class="form-group form-forget">
-								<div class="custom-control custom-checkbox">
-									<input type="checkbox" class="custom-control-input" id="customControlAutosizing">
-									<label class="custom-control-label" for="customControlAutosizing">Remember me</label>
-								</div>
-								<a href="forgot-password.php" class="ml-auto">Forgot Password?</a>
 							</div>
 						</div>
 						<div class="col-lg-12 m-b30">
-							<button name="login_btn" type="submit" value="Submit" class="btn button-md">Login</button>
+							<button name="submit" type="submit" name="resetbtn" value="Submit" class="btn button-md">Submit</button>
 						</div>
-						<!-- <div class="col-lg-12">
-							<h6>Login with Social media</h6>
-							<div class="d-flex">
-								<a class="btn flex-fill m-r5 facebook" href="#"><i class="fa fa-facebook"></i>Facebook</a>
-								<a class="btn flex-fill m-l5 google-plus" href="#"><i class="fa fa-google-plus"></i>Google Plus</a>
-							</div>
-						</div> -->
 					</div>
 				</form>
 			</div>
@@ -161,9 +114,6 @@ if(isset($_SESSION['auth']))
 <script src="assets/js/functions.js"></script>
 <script src="assets/js/contact.js"></script>
 <script src='assets/vendors/switcher/switcher.js'></script>
-
-<script>document.addEventListener('contextmenu', event => event.stopPropagation(), true);</script>
-
 </body>
 
 </html>
--- password-change.php
+++ password-change.php
@@ -2,9 +2,42 @@
 session_start();
 include('db_conn.php');
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
 function send_password_reset($get_name,$get_email,$token)
 {
-    
+  $mail = new PHPMailer(true);
+  // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                    
+  $mail->isSMTP();                                            
+  $mail->SMTPAuth   = true;                                   
+
+  $mail->Host       = "smtp.gmail.com";                     
+  $mail->Username   = "edukwicorg@gmail.com";                     
+  $mail->Password   = "***";                               
+
+  $mail->SMTPSecure = "tls"; 
+  
+  $mail->Port       = 587;                                   
+
+  
+  $mail->setFrom("edukwicorg@gmail.com", $get_name);
+  $mail->addAddress($get_email);     
+ 
+
+  $mail->isHTML(true);                                  
+  $mail->Subject = "Reset Password Notification";
+  //Attachments
+  $mail->Body    = "<h2>Hello!</h2>
+                    <h3>You are receiving this email because we received a password reset request for your account<h3>
+                    <br>
+                    <a href='http://localhost/EDUKWIC-website/password-reset-page.php?token=$token&email=$get_email'>Reset Password</a>";
+                    
+   $mail->send();                
 }
 
 if(isset($_POST['resetbtn']))
@@ -18,27 +51,27 @@ if(isset($_POST['resetbtn']))
    if(mysqli_num_rows($check_email_run)>0)
    {
      $row = mysqli_fetch_array($check_email_run);
-     $get_name = $row['name'];
-     $get_email = $row['email'];
+     $get_name = $row['user_name'];
+     $get_email = $row['user_email'];
 
      $update_token = "UPDATE users SET verify_token='$token' WHERE user_email='$get_email' LIMIT 1";
      $update_token_run = mysqli_query($conn, $update_token);
        if($update_token_run)
        {
          send_password_reset($get_name,$get_email,$token);
-         $_SESSION['status'] = "We emailed you a password reset link";
-         header("Location: forget-password.html");
+        //  $_SESSION['status'] = "We emailed you a password reset link";
+         header("Location: forget-password.php");
         exit(0);
        }
        else{
-        $_SESSION['status'] = "Something went wrong #1";
-    header("Location: forget-password.html");
+        // $_SESSION['status'] = "Something went wrong #1";
+    header("Location: forget-password.php");
     exit(0);
        }
    }
-   else{
-    $_SESSION['status'] = "No Email Found";
-    header("Location: forget-password.html");
+ else{
+    // $_SESSION['status'] = "No Email Found";
+    header("Location: forget-password.php");
     exit(0);
    }
 }
